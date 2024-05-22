ad985b1b95d62347e2fc268c3552f986c57d8f7d
Evan <evant5252@gmail.com>
2024-05-22 11:28:39 -0700

    web app complete

--- server.py
+++ server.py
@@ -3,16 +3,21 @@ from flask_cors import CORS
 import pickle
 import numpy as np
 
+# ----------------- Setup -----------------
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# Load the model
 with open('log_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
+# ----------------- Routes -----------------
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -20,6 +25,8 @@ def predict():
     features = np.array(data['features']).reshape(1, -1)
     prediction = model.predict(features)
     return jsonify({'prediction': prediction.tolist()})
+
+# ----------------- Run -----------------
 
 if __name__ == '__main__':
     app.run(debug=True)
